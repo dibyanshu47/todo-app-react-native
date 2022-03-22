0980c5b8f10189e657ac62c4f1a16a523e1602a8
Dibyanshu Jaiswal <uname2334@gmail.com>
2022-03-22 18:07:02 +0530

    implemented async storage

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard, AppState } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Header from './components/header';
 import TodoItem from './components/todoItem';
@@ -8,25 +9,75 @@ import AddTodo from './components/addTodo';
 import NoTodo from './components/noTodo';
 
 export default function App() {
+
     const [todos, setTodos] = useState([]);
 
+    useEffect(() => {
+        const fetchTodos = () => {
+            AsyncStorage.getItem('todos').then(data => {
+                setTodos((prevTodos) => {
+                    if (data !== null) {
+                        return [...prevTodos, ...JSON.parse(data)];
+                    } else {
+                        return [...prevTodos];
+                    }
+                })
+            }).catch((error) => console.log(error));
+        };
+        fetchTodos();
+    }, [])
+
+    useEffect(() => {
+        console.log("UPDAGTE");
+        const fetchTodos = async () => {
+            try {
+                await AsyncStorage.setItem('todos', JSON.stringify(todos));
+                // debugTodo();
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchTodos();
+    }, [todos])
+
+
+    const debugTodo = () => {
+        AsyncStorage.getItem('todos').then(data => {
+            console.log(JSON.parse(data));
+        }).catch(error => console.log(error));
+    }
+
+    const updateTodos = async () => {
+        try {
+            await AsyncStorage.setItem('todos', JSON.stringify(todos));
+            // debugTodo();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     const pressHandler = (key) => {
+        // console.log(todos);
         setTodos((prevTodos) => {
             return prevTodos.filter(todo => todo.key != key);
         });
+        // console.log(todos);
     }
 
     const submitHandler = (text) => {
-
-        if (text.length > 3) {
+        if (text.length > 2) {
+            console.log("UP");
+            console.log(todos);
             setTodos((prevTodos) => {
                 return [
                     { text: text, key: Math.random().toString() },
                     ...prevTodos
                 ]
             })
+            console.log("DOWN");
+            console.log(todos);
         } else {
-            Alert.alert('OOPS!', 'Todos must be over 3 characters long.', [
+            Alert.alert('OOPS!', 'Todos must be over 2 characters long.', [
                 { text: 'Understood', onPress: () => console.log('alert closed') }
             ]);
         }
